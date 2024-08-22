fc69d592e9c431fb767d8a9f99181acabd1e8843
althafabdulraheem <althafbinraheem786@gmail.com>
2024-08-22 23:25:41 +0530

    Login page design changes + authentication started

---
+++ src/hooks/AuthFunction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const useAuthHook=()=>{
+   const Login=(email,password)=>{
+        try{
+
+        }
+        catch(error)
+        {
+            
+        }
+   }
+
+   
+}
+
+export default useAuthHook;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Login.css'
 const Login=()=>{
+
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
     return (<>
-        <h1>Login</h1>
-        <div className="login-form-wrapper" >
+        <h1>Scrum Mater</h1>
+        <div className="login-form-wrapper">
+            <h1>Login</h1>
             <div className="form-group">
-                <input type="text"  class="form-field" name="email" placeholder='enter email' autocomplete="new-password"/>
+                <input type="text"  onChange={(e)=>setEmail(e.target.value)} class="form-field" name="email" placeholder='enter email' autocomplete="new-password"/>
             </div>
             <div className="form-group">
-                <input type="password" class="form-field" name="password" placeholder='enter password' autocomplete="new-password"/>
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} class="form-field" name="password" placeholder='enter password' autocomplete="new-password"/>
             </div>
             <div className="btn-wrapper">
                 <button>submit</button>
